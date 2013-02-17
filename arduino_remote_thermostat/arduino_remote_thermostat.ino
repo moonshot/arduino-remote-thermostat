3c0ac8b0c2354515a4ad1ee7c0a55e8e4e379957
Moon Shot <moonshot2x@gmail.com>
2013-02-17 15:55:02 -0500

    update with working code

--- arduino_remote_thermostat/arduino_remote_thermostat.ino
+++ arduino_remote_thermostat/arduino_remote_thermostat.ino
@@ -1,11 +1,12 @@
 int sensor_pin = 5; // TMP36 Vout pin (analog)
 int off_switch = 4; // to trigger an OFF signal
 int on_switch = 2; // to trigger an ON signal
-int too_cold_temp = 65;
-int too_hot_temp = 75;
+float too_cold_temp = 65.0;
+float too_hot_temp = 75.0;
 int too_cold_led = 9;
 int too_hot_led = 11;
 int ok_led = 10;
+boolean debug_mode = false;
 
 void setup()
 {
@@ -22,6 +23,10 @@ void setup()
   digitalWrite(off_switch, HIGH);
   delay(200);
   digitalWrite(off_switch, LOW);
+  
+  if (debug_mode) {
+    Serial.begin(9600);
+  }
 }
 
 void loop()
@@ -30,15 +35,24 @@ void loop()
   int reading = analogRead(sensor_pin);
 
   // convert that reading to degrees Farenheit
-  float voltage = reading * 5.0 / 1024.0;
-  float degrees_f = (((voltage - 0.5) * 100) * 9.0 / 5.0) + 32.0;
+  float voltage = reading * 5.0;
+  voltage /= 1024.0;
+  float degrees_c = (voltage - 0.5) * 100;
+  float degrees_f = (degrees_c * 9.0 / 5.0) + 32.0;
+  if (debug_mode) {
+    Serial.print(degrees_f);
+  }
 
   // if it's too cold, then push the on button
   if (degrees_f < too_cold_temp) {
+    if (debug_mode) {
+      Serial.println(" too cold");
+    }
     // push the on button
     digitalWrite(on_switch, HIGH);
     delay(200);
     digitalWrite(on_switch, LOW);
+
     digitalWrite(too_cold_led, HIGH);
     digitalWrite(too_hot_led, LOW);
     digitalWrite(ok_led, LOW);
@@ -46,9 +60,14 @@ void loop()
 
   // if it's too hot, then push the off button
   else if (degrees_f > too_hot_temp) {
+    if (debug_mode) {
+      Serial.println(" too hot");
+    }
+
     digitalWrite(off_switch, HIGH);
     delay(200);
     digitalWrite(off_switch, LOW);
+    
     digitalWrite(too_cold_led, LOW);
     digitalWrite(too_hot_led, HIGH);
     digitalWrite(ok_led, LOW);
@@ -57,6 +76,10 @@ void loop()
   // temp is just right
   // heating pad can be either on or off
   else {
+    if (debug_mode) {
+      Serial.println(" just right");
+    }
+
     digitalWrite(too_cold_led, LOW);
     digitalWrite(too_hot_led, LOW);
     digitalWrite(ok_led, HIGH);
